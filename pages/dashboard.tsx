d2076cd5d033d8d3795af84988af8238be25ddfc
philanikhumalokzn <philani.khumalo@kznschools.gov.za>
2026-01-18 22:32:22 +0200

    style: add 'dashboard-flat' class for improved layout in mobile and desktop views

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -7627,7 +7627,7 @@ export default function Dashboard() {
       <div
         className={
           isMobile
-            ? 'relative z-10 w-full px-2 min-h-[100dvh] flex flex-col'
+            ? 'relative z-10 w-full px-2 min-h-[100dvh] flex flex-col dashboard-flat'
             : 'max-w-5xl mx-auto px-4 lg:px-8 py-8 space-y-6 dashboard-flat'
         }
       >
